c36ed0cc6f80f533a817250f6ac04b77181825d0
Cr1pto9 <84429701+Cr1pto9@users.noreply.github.com>
2021-05-27 18:37:48 +0000

    Update and rename loader.sh to load.sh

---
+++ load.sh
@@ -0,0 +1,21 @@
+
+#loader by cripto9
+echo -e '\e[1;36mCargando...'
+echo -e '¶¶¶¶5%......................'
+sleep 0.1
+clear
+echo -e 'Cargando...\n¶¶¶¶¶¶¶¶8%...................'
+sleep 0.2
+clear
+echo -e 'Cargando...\n¶¶¶¶¶¶¶¶¶¶¶¶15%..............'
+sleep 0.3
+clear
+echo -e 'Cargando...\n¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶21%........'
+sleep 0.3
+clear
+echo -e '\cargando...\n¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶26%..'
+sleep 0.1
+clear
+echo -e '\Cargando...\n¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶'
+sleep 0.1
+clear
